4457135dcb2630c2bedd86c61ac1efda921d0028
paschal <okwuosahpaschal@gmail.com>
2020-11-01 15:23:02 +0000

    Updates

---
+++ precache-manifest.1bb6a3e23acc8eca46e480587e39975c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "260aa3b742efc92b5b08d4c4dba0c839",
+    "url": "/tracemovies/index.html"
+  },
+  {
+    "revision": "fed3dc6a2169a83d08f8",
+    "url": "/tracemovies/static/css/2.44309f2b.chunk.css"
+  },
+  {
+    "revision": "5dd8c59cc823da0ddcb6",
+    "url": "/tracemovies/static/css/main.aaf0641a.chunk.css"
+  },
+  {
+    "revision": "fed3dc6a2169a83d08f8",
+    "url": "/tracemovies/static/js/2.ec3ffea2.chunk.js"
+  },
+  {
+    "revision": "bfa109f112a9f8af844960a54c8d9cd7",
+    "url": "/tracemovies/static/js/2.ec3ffea2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5dd8c59cc823da0ddcb6",
+    "url": "/tracemovies/static/js/main.a612354c.chunk.js"
+  },
+  {
+    "revision": "88a8b1e2b807004a5ff3",
+    "url": "/tracemovies/static/js/runtime-main.fb8d5a57.js"
+  }
+]);
