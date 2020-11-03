57cf3fc7676b722d93c440e63d79364a7c3582b4
paschal <okwuosahpaschal@gmail.com>
2020-11-03 02:55:48 +0000

    Updates

--- precache-manifest.413fc67fbe547b8f211d956f7efc4b1f.js
+++ precache-manifest.413fc67fbe547b8f211d956f7efc4b1f.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e387995db64e502c1a8fe4252e90d89b",
+    "revision": "6336d0da8a8ddd484702c9595399f708",
     "url": "/tracemovies/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/tracemovies/static/css/2.44309f2b.chunk.css"
   },
   {
-    "revision": "d5aafe70778408cf521b",
+    "revision": "5228eccc5632347f4a29",
     "url": "/tracemovies/static/css/main.0e16ba85.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/tracemovies/static/js/2.ec3ffea2.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "d5aafe70778408cf521b",
-    "url": "/tracemovies/static/js/main.e79d5e85.chunk.js"
+    "revision": "5228eccc5632347f4a29",
+    "url": "/tracemovies/static/js/main.9a54afe9.chunk.js"
   },
   {
     "revision": "88a8b1e2b807004a5ff3",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/tracemovies/precache-manifest.90ad07e10fefadfd6fdef9cc09162347.js"
+  "/tracemovies/precache-manifest.413fc67fbe547b8f211d956f7efc4b1f.js"
 );
 
 self.addEventListener('message', (event) => {
